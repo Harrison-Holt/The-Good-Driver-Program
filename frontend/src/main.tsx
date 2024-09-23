3da304d815c61beef1f615ac96e3bd7546600d05
hsholt <hsholt@clemson.edu>
2024-09-23 14:57:06 -0400

    Added About Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { createBrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 const router = createBrowserRouter([
     {
