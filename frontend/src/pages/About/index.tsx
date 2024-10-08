b66fe64cd75f01c6257f179d15f2a3ebfc43a919
addis0n1 <addis0n1@aol.com>
2024-10-08 15:22:34 -0400

    fixed json parsing

--- frontend/src/pages/About/index.tsx
+++ frontend/src/pages/About/index.tsx
@@ -21,7 +21,7 @@ const About: React.FC = () => {
         let data = await response.json();
 
         // Parse the `body` which contains the actual JSON data
-        data = JSON.parse(result.body);
+        data = JSON.parse(data.body);
 
         console.log('API Response:', data); // Log the response to inspect it
         setAboutData(data);
