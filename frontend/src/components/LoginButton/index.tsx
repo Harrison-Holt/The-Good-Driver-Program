9e6dacebaab2597a583de2d6573b2eb31ecf7782
Tradd Thomas <traddt@clemson.edu>
2024-10-10 17:45:42 -0400

    login redirect fix

--- frontend/src/components/LoginButton/index.tsx
+++ frontend/src/components/LoginButton/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material"
-import { useNavigate} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
 import { selectLogin } from "../../store/userSlice";
 
@@ -18,7 +18,9 @@ const LoginButton: React.FC = () => {
         <>
             <Button
                 variant="contained"
-                onClick={/*Navigate to login page*/()=>{nav("https://team08-domain.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=ff8qau87sidn42svsuj51v4l4&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2FTeam08-Callback")}}
+                onClick={/*Navigate to login page*/()=>{
+                    window.location.href="https://team08-domain.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=ff8qau87sidn42svsuj51v4l4&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2FTeam08-Callback"
+                }}
             >
                 {buttonMessage}
             </Button>
