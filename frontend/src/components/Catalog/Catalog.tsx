fd02504a410205cd9fe8c467e270fd972519cd3e
hsholt <hholt2901@gmail.com>
2024-10-14 17:30:19 -0400

    1

--- frontend/src/components/Catalog/Catalog.tsx
+++ frontend/src/components/Catalog/Catalog.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import CatalogItem from './CatalogItem';
 import SearchBar from '../SearchBar/index';
 
--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -7,26 +7,26 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ setSearchTerm }) => {
   return (
-    <>
-      <Autocomplete
-        id="search-bar"
-        freeSolo
-        disableClearable
-        options={["placeholder", "replace with real options", 'item']} // You can make this dynamic later
-        onInputChange={(event, value) => setSearchTerm(value)} // Update search term
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search input"
-            InputProps={{
-              ...params.InputProps,
-              type: 'search',
-            }}
-          />
-        )}
-      />
-    </>
+    <Autocomplete
+      id="search-bar"
+      freeSolo
+      disableClearable
+      options={["placeholder", "replace with real options", 'item']}
+      onInputChange={(_, value) => setSearchTerm(value)} 
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search input"
+          InputProps={{
+            ...params.InputProps,
+            type: 'search',
+          }}
+        />
+      )}
+    />
   );
 };
 
 export default SearchBar;
+
+
