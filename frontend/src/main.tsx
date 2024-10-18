62f948c0ca0fa8991a90705811c5b4851abee9fc
hsholt <hholt2901@gmail.com>
2024-10-18 10:31:57 -0400

    attempted to add product page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { createBrowserRouter } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
+import Catalog from './components/Catalog/Catalog';
+import ProductDetails from './components/Catalog/ProductDetails'; // Import ProductDetails
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>
+        element: <Home />
     },
     {
         path: "/about",
-        element: <About/>
+        element: <About />
+    },
+    {
+        path: "/catalog",  
+        element: <Catalog />
+    },
+    {
+        path: "/product/:itemId", 
+        element: <ProductDetails />
     }
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={ router } />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
-)
+);
