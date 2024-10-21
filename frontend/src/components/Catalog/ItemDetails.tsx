02b024b0e2b215e6180aa7f270ffa6a740e7af44
hsholt <hholt2901@gmail.com>
2024-10-20 21:51:07 -0400

    attempted to add product page

--- frontend/src/components/Catalog/ItemDetails.tsx
+++ frontend/src/components/Catalog/ItemDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Typography, CircularProgress } from '@mui/material';
 
