b736b20f7f7b4e7d439036b1e4727e07bb681457
hsholt <hholt2901@gmail.com>
2024-10-26 16:54:21 -0400

    added user reviews

--- frontend/src/components/Catalog/Catalog.tsx
+++ frontend/src/components/Catalog/Catalog.tsx
@@ -41,7 +41,7 @@ const categories = [
 ];
 
 const API_BASE_URL = 'https://itunes.apple.com/search';
-const REVIEW_API_URL = 'https://5w209ckis5.execute-api.us-east-1.amazonaws.com/prod/dev';
+const REVIEW_API_URL = 'https://dtnha4rfd4.execute-api.us-east-1.amazonaws.com/dev/reviews';
 
 const Catalog = () => {
   const [items, setItems] = useState<ItunesItem[]>([]);
@@ -124,7 +124,7 @@ const handleSubmitReview = async () => {
       const itemId = selectedItem.trackId || selectedItem.collectionId;
 
       console.log(itemId); 
-      
+
       if (!itemId) {
         setError("Item ID is missing, cannot submit review.");
         return;
