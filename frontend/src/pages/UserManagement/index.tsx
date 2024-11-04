f25c6b84c98b5ca368329438b45fd7bc3e7cba21
addis0n1 <addis0n1@aol.com>
2024-11-04 14:57:04 -0500

    Added frontend components for creating sponsor org

--- frontend/src/pages/UserManagement/index.tsx
+++ frontend/src/pages/UserManagement/index.tsx
@@ -1,8 +1,23 @@
+import { TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import SearchBar from "../../components/SearchBar";
 
 const UserManagement: React.FC = () => {
 
+    const [orgName, setOrgName] = useState("")
+    const [initialSponsorUser, setInitialSponsorUser] = useState("")
+
     return (
         <>
+            <Typography>Create Sponsor Organization</Typography>
+            <TextField
+                label={"Organization Name"}
+                value={orgName}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setOrgName(event.target.value);
+                }}
+            />
+            <SearchBar setSearchTerm={setInitialSponsorUser} options={[""]} label="Select Sponsor User" />
         </>
     )
 }
