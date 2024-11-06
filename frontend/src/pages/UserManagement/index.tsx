3be434a7ee46f078bf7b44d0254735c3434335d8
addis0n1 <addis0n1@aol.com>
2024-11-06 14:11:56 -0500

    Added function to create new sponsor organizations

--- frontend/src/pages/UserManagement/index.tsx
+++ frontend/src/pages/UserManagement/index.tsx
@@ -1,11 +1,25 @@
-import { TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import SearchBar from "../../components/SearchBar";
+import axios from "axios";
 
 const UserManagement: React.FC = () => {
 
     const [orgName, setOrgName] = useState("")
     const [initialSponsorUser, setInitialSponsorUser] = useState("")
+
+    const createSponsorOrg = async () => {
+        try {
+            axios.post('https://0w2ntl28if.execute-api.us-east-1.amazonaws.com/dec-db/sponsors', {
+                org_name: orgName,
+                initial_sponsor_user: initialSponsorUser
+            }).then((response) => {
+                console.log(response);
+            });
+        } catch (error) {
+            console.error('Error creating sponsor:', error);
+        }
+    }
 
     return (
         <>
@@ -18,6 +32,7 @@ const UserManagement: React.FC = () => {
                 }}
             />
             <SearchBar setSearchTerm={setInitialSponsorUser} options={[""]} label="Select Sponsor User" />
+            <Button onClick={() => {createSponsorOrg()}}>Submit</Button>
         </>
     )
 }
