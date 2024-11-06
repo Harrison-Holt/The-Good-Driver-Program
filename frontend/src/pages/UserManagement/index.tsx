5cb0eaa24c1ed92bcc1c32bdf60e962619ab655e
addis0n1 <addis0n1@aol.com>
2024-11-06 14:35:19 -0500

    Cleaned up sponsor creation/broke out into separate component

--- frontend/src/pages/UserManagement/index.tsx
+++ frontend/src/pages/UserManagement/index.tsx
@@ -1,60 +1,15 @@
-import { Button, TextField, Typography } from "@mui/material";
-import { useState, useEffect } from "react";
-import SearchBar from "../../components/SearchBar";
-import axios from "axios";
 import Navibar from "../../components/Navibar";
+import CreateSponsorOrg from "../../components/CreateSponsorOrg";
+import { Box } from "@mui/material";
 
 const UserManagement: React.FC = () => {
-
-    const [orgName, setOrgName] = useState("")
-    const [initialSponsorUser, setInitialSponsorUser] = useState("")
-    const [userList, setUserList] = useState([]);
-
-    const createSponsorOrg = async () => {
-        try {
-            axios.post('https://0w2ntl28if.execute-api.us-east-1.amazonaws.com/dec-db/sponsors', {
-                org_name: orgName,
-                initial_sponsor_user: initialSponsorUser
-            }).then((response) => {
-                console.log(response);
-            });
-        } catch (error) {
-            console.error('Error creating sponsor:', error);
-        }
-    }
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('https://0w2ntl28if.execute-api.us-east-1.amazonaws.com/dec-db/drivers');
-            let data = await response.json();
-
-            // Parse the `body` which contains the actual JSON data
-            data = JSON.parse(data.body);
-
-            console.log('API Response:', data); // Log the response to inspect it
-            setUserList(data);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <>
             <Navibar/>
-            <Typography>Create Sponsor Organization</Typography>
-            <TextField
-                label={"Organization Name"}
-                value={orgName}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    setOrgName(event.target.value);
-                }}
-            />
-            <SearchBar setSearchTerm={setInitialSponsorUser} options={userList} label="Select Sponsor User" />
-            <Button onClick={() => {createSponsorOrg()}}>Submit</Button>
+            <Box>
+                <CreateSponsorOrg/>
+            </Box>
         </>
     )
 }
